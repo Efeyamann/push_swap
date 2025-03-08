a3d1ddb874af2bbef5d446d63f75023c6c4e8efd
Efe Yaman Sir <esir@k1m41s04.42istanbul.com.tr>
2025-03-08 14:53:44 +0300

    operations done1

--- push_swap.h
+++ push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: efe <efe@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: esir <esir@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 13:24:50 by efe               #+#    #+#             */
-/*   Updated: 2025/03/01 15:18:49 by efe              ###   ########.fr       */
+/*   Updated: 2025/03/08 14:05:47 by esir             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,19 @@ typedef struct s_node
 	struct s_node	*next;
 }	t_node;
 
-void	checker(char **argv);
 t_node	*create_list(int argc, char *argv[]);
-int	is_duplicate(t_node *stack, int num);
+int		is_duplicate(t_node *stack, int num);
+void	checker(char **argv);
+void	sa(t_node **stack_a);
+void	sb(t_node **stack_b);
+void	ss(t_node **stack_a, t_node **stack_b);
+void	pa(t_node **stack_a, t_node **stack_b);
+void	pb(t_node **stack_a, t_node **stack_b);
+void	ra(t_node **stack_a);
+void	rb(t_node **stack_b);
+void	rr(t_node **stack_a, t_node **stack_b);
+void	rra(t_node **stack_a);
+void	rrb(t_node **stack_b);
+void	rrr(t_node **stack_a, t_node **stack_b);
 
 #endif
--- reverse_rotate.c
+++ reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: esir <esir@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 13:41:25 by esir              #+#    #+#             */
-/*   Updated: 2025/03/08 13:43:10 by esir             ###   ########.fr       */
+/*   Updated: 2025/03/08 14:01:56 by esir             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,46 @@ void	rra(t_node **stack_a)
 {
 	t_node	*temp;
 	t_node	*last;
+	t_node	*prev;
+
+	if (!stack_a || !(*stack_a) || !((*stack_a)->next))
+		return ; 
+	temp = *stack_a;
+	prev = NULL;
+	last = *stack_a;
+	while (last > prev)
+	{
+		prev = last;
+		last = last->next;
+	}
+	prev->next = NULL;
+	last->next = temp;
+	*stack_a = last;
+}
+
+void	rrb(t_node **stack_b)
+{
+	t_node	*temp;
+	t_node	*last;
+	t_node	*prev;
+
+	if (!stack_b || !(*stack_b) || !((*stack_b)->next))
+		return ; 
+	temp = *stack_b;
+	prev = NULL;
+	last = *stack_b;
+	while (last > prev)
+	{
+		prev = last;
+		last = last->next;
+	}
+	prev->next = NULL;
+	last->next = temp;
+	*stack_b = last;
+}
+
+void	rrr(t_node **stack_a, t_node **stack_b)
+{
+	rra(stack_a);
+	rrb(stack_b);
 }
