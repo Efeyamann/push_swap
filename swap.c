46b95d7f253631485068139b2337009294545db3
Efe Yaman Sir <esir@k1m41s04.42istanbul.com.tr>
2025-03-08 13:03:56 +0300

    swap functions

---
+++ swap.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esir <esir@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/01 13:24:39 by efe               #+#    #+#             */
+/*   Updated: 2025/03/08 13:01:19 by esir             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sa(t_node **stack_a)
+{
+	t_node	*first;
+	t_node	*second;
+
+	
+	if (!stack_a || !(*stack_a) || !((*stack_a)->next))
+		return ;
+	first = *stack_a;
+	second = first->next;
+
+	first->next = second->next;
+	second->next = first;
+	*stack_a = second;
+}
+
+void	sb(t_node **stack_b)
+{
+	t_node	*first;
+	t_node	*second;
+
+	
+	if (!stack_b || !(*stack_b) || !((*stack_b)->next))
+		return ;
+	first = *stack_b;
+	second = first->next;
+
+	first->next = second->next;
+	second->next = first;
+	*stack_b = second;	
+}
+
+void	ss(t_node **stack_a, t_node **stack_b)
+{
+	sa(stack_a);
+	sb(stack_b);
+}
