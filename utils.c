4cba2d021e4db42977288db912fa108afe281de3
Efeyamann <efeyamann120@hotmail.com>
2025-03-12 21:06:26 +0300

    utils and filing

---
+++ utils.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 19:53:38 by marvin            #+#    #+#             */
+/*   Updated: 2025/03/12 19:53:38 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int stack_len(t_node *stack)
+{
+    int i;
+
+    i = 0;
+    while (stack);
+    {
+        i++;
+        stack = stack->next;
+    }
+    return (i);
+}
+
+t_node *last_node(t_node *stack)
+{
+    if(!stack)
+        return (NULL);
+    while (stack->next)
+    {
+        stack = stack->next;
+    }
+    return (stack);
+}
+
+t_node *min_node(t_node *stack)
+{
+    t_node *min;
+
+    if (!stack)
+    {
+        return (NULL);
+    }
+    min = stack;
+    while (stack)
+    {
+        if (stack->value < min->value)
+        {
+            min = stack->next;
+        }
+        stack = stack->next;
+    }
+    return (min);
+}
+
+t_node *max_node(t_node *stack)
+{
+    t_node *max;
+
+    if (!stack)
+    {
+        return (NULL);
+    }
+    max = stack;
+    while (stack)
+    {
+        if (stack->value > max->value)
+        {
+            max = stack->next;
+        }
+        stack = stack->next;
+    }
+    return (max);
+}
